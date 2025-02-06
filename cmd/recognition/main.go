ed539bdd9bed5f1e1d6364d0aa86d7f200ea76e0
mushoffa <ridwan.mushoffa@gmail.com>
2025-02-06 23:30:38 +0700

    feat: Add example code

--- cmd/recognition/main.go
+++ cmd/recognition/main.go
@@ -9,15 +9,27 @@ import (
 )
 
 func main() {
-	filename := "or_16214_p.2_line_01.png"
-	threshold := uint8(180)
+	filename := "OR_16214_P001L0101_W_202.png"
+	tessdata := "/media/mushoffa/DATA/4_WORK/2_UTM/1_JAWI_OCR/2_CODE/jawi-ocr/recognition/model/"
+	// threshold := uint8(180)
 
 	input, err := image.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
 
-	m := image.New(input).Monochrome(threshold, false)
-	res := recognition.New(m)
+	// m := image.New(input).Monochrome(threshold, false)
+	m := image.New(input)
+	img, err := m.Bytes()
+	if err != nil {
+		panic(err)
+	}
+
+	client := recognition.NewTesseract(tessdata)
+	res, err := client.Recognize("jawi", img)
+	if err != nil {
+		panic(err)
+	}
+	// res := recognition.NewWithDataset(input, "jawi")
 	fmt.Println("Recognition: ", res)
 }
